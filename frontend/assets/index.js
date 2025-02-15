a51575a631d5317a7f722295c28719fb09e21ca8
Mrigayan <connect.mrigayan@gmail.com>
2025-02-15 13:27:15 +0530

    Changes

--- frontend/assets/index.js
+++ frontend/assets/index.js
@@ -1,4 +1,10 @@
 var data;
+var current = 0;
+var max;
+
+const videoRef = document.getElementById('videoPlayer');
+const loader = document.getElementById('loader');
+const queue = document.querySelector('.queue');
 
 function handleKeydown(event) {
     var command;
@@ -21,64 +27,84 @@ function handleKeydown(event) {
         default:
             break;
     }
-
-    document.getElementById("hello").textContent = command;
 }
 
-document.addEventListener('DOMContentLoaded', async() => {
+function errorNotify(message) {
+
+}
+
+function setLoading(status) {
+    loader.style.display = (status) ? 'block' : 'none';
+};
+
+function setController(element) {
+    if (element === "queue") {
+        data.forEach(element => {
+            const blockDiv = document.createElement('div');
+            blockDiv.className = 'block';
+            const titleDiv = document.createElement('div');
+            titleDiv.className = 'title';
+            titleDiv.textContent = element["song_title"];
+            const artistDiv = document.createElement('div');
+            artistDiv.className = 'artist';
+            artistDiv.textContent = element["artist"];
+            blockDiv.appendChild(titleDiv);
+            blockDiv.appendChild(artistDiv);
+            queue.appendChild(blockDiv);
+        });
+    }
+}
+
+function setVideo(video) {
+    videoRef.src = `http://rythmebox.mrigyn.shop/play?id=${video["_id"]}`;
+    fetch(`http://rythmebox.mrigyn.shop/play?id=${video["_id"]}`)
+        .then(response => {
+            if (response.status === 200) {
+                const player = dashjs.MediaPlayer().create();
+                player.initialize(videoRef, `http://rythmebox.mrigyn.shop/play/manifest.mpd`, true);
+            } else {
+                throw new Error('Video not found');
+            }
+        })
+        .catch(error => {
+            errorNotify(`Error While Fetching Video : ${error}`);
+        });
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
     try {
         const response = await fetch("http://rythmebox.mrigyn.shop/random");
         const result = await response.json();
         data = result;
-        setVideo(result[0]);
+        setController("queue");
+        max = Object.values(result).length;
+        setVideo(result[current]);
     } catch (error) {
-        console.error("Error fetching data:", error);
+        errorNotify(`Error While Fetching Data : ${error}`);
+    }
+
+    videoRef.addEventListener('loadeddata', () => setLoading(false));
+    videoRef.addEventListener('waiting', () => setLoading(true));
+    videoRef.addEventListener('stalled', () => setLoading(true));
+    videoRef.addEventListener('play', () => setLoading(false));
+    videoRef.addEventListener('pause', () => setLoading(false));
+    videoRef.addEventListener('abort', () => setLoading(true));
+    videoRef.addEventListener('emptied', () => setLoading(true));
+    videoRef.addEventListener('error', () => setLoading(true));
+    videoRef.addEventListener('suspend', () => setLoading(true));
+    videoRef.addEventListener('ended', () => setVideo(data[(++current) % max]));
+});
+
+document.addEventListener('keypress', (event) => {
+    if (event.key === 'p') {
+        if (videoRef.paused) {
+            videoRef.play();
+        } else {
+            videoRef.pause();
+        }
     }
 });
 
-
-function setVideo(video) {
-    const videoRef = document.getElementById('videoPlayer');
-    videoRef.src = `http://rythmebox.mrigyn.shop/play?id=${video["_id"]}`; // Assuming 'video' has a 'url' property
-
-
-    const loader = document.getElementById('loader');
-
-    const setLoading = (loading) => {
-        loader.style.display = loading ? 'block' : 'none';
-    };
-
-    const initializePlayer = (videoId) => {
-        fetch(`http://rythmebox.mrigyn.shop/play?id=${videoId}`)
-            .then(response => {
-                if (response.status === 200) {
-                    return ""; // response.json();
-                } else {
-                    throw new Error('Video not found');
-                }
-            })
-            .then(() => {
-                // Initialize dash.js player
-                const player = dashjs.MediaPlayer().create();
-                player.initialize(videoRef, `http://rythmebox.mrigyn.shop/play/manifest.mpd`, true);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                setLoading(false);
-            });
-    };
-
-    initializePlayer(video["_id"]);
-
-    videoRef.addEventListener('loadeddata', () => setLoading(false));
-    videoRef.addEventListener('play', () => setLoading(false));
-    videoRef.addEventListener('pause', () => setLoading(false));
-
-    // Cleanup event listeners when necessary
-    window.addEventListener('beforeunload', () => {
-        videoRef.removeEventListener('loadeddata', () => setLoading(false));
-        videoRef.removeEventListener('play', () => setLoading(false));
-        videoRef.removeEventListener('pause', () => setLoading(false));
-    })
-};
+window.addEventListener('beforeunload', () => {
+    videoRef.removeEventListener('loadeddata', () => setLoading());
+})
